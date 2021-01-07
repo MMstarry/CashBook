98f25651e2cbafe460e8ae66878f124ab77ac0ea
1668846604@qq.com <1668846604@qq.com>
2021-01-07 16:18:13 +0800

    添加项目github地址

--- app/src/main/java/com/xuexiang/qqgg/fragment/AboutFragment.java
+++ app/src/main/java/com/xuexiang/qqgg/fragment/AboutFragment.java
@@ -17,10 +17,14 @@
 
 package com.xuexiang.qqgg.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.TextView;
 
 import com.xuexiang.qqgg.R;
 import com.xuexiang.qqgg.core.BaseFragment;
+import com.xuexiang.qqgg.utils.Utils;
+import com.xuexiang.qqgg.utils.XToastUtils;
 import com.xuexiang.qqgg.utils.sdkinit.XUpdateInit;
 import com.xuexiang.xpage.annotation.Page;
 import com.xuexiang.xui.widget.grouplist.XUIGroupListView;
@@ -51,11 +55,24 @@ public class AboutFragment extends BaseFragment {
         XUIGroupListView.newSection(getContext())
                 .addItemView(mAboutGroupListView.createItemView("版本更新"), v -> XUpdateInit.checkUpdate(getContext(), true))
                 .addItemView(mAboutGroupListView.createItemView("赞助项目"), v -> openPage(SponsorFragment.class))
+                .addItemView(mAboutGroupListView.createItemView("作者QQ"), v -> addQQ())
+                .addItemView(mAboutGroupListView.createItemView("GitHub主页"), v -> Utils.goWeb(getContext(), "https://github.com/MMstarry/CashBook/"))
                 .addTo(mAboutGroupListView);
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy", Locale.CHINA);
         String currentYear = dateFormat.format(new Date());
         mCopyrightTextView.setText(String.format(getResources().getString(R.string.about_copyright), currentYear));
 
+    }
+
+    public void addQQ(){
+        try {
+            //第二种方式：可以跳转到添加好友，如果qq号是好友了，直接聊天
+            String url = "mqqwpa://im/chat?chat_type=wpa&uin=1668846604";//uin是发送过去的qq号码
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            XToastUtils.info("请检查是否安装QQ");
+        }
     }
 }
